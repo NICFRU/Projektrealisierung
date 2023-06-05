7eb792511fcb60f39779814456988447b9579d94
NICFRU <niclascramer@gmail.com>
2023-06-05 21:10:47 +0200

    speech to text

--- main.py
+++
@@ -1,9 +0,0 @@
-import speech_recognition as sr
-
-r=sr.Recognizer()
-
-with sr.Microphone() as source:
-    print('Sy something')
-    audio=r.listen(source)
-    voice_data=r.recognize_google(audio)
-    print(voice_data)
