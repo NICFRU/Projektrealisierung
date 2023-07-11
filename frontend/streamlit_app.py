2cab65ead6d33661f44c23a726363aafd52d8783
Jasmina11 <77899903+Jasmina11@users.noreply.github.com>
2023-07-12 00:56:49 +0200

    Create streamlit_app.py
    frontend

---
+++ frontend/streamlit_app.py
@@ -0,0 +1,79 @@
+import streamlit as st
+
+# Hintergrundfarbe und Schriftfarbe einstellen
+st.markdown(
+    """
+    <style>
+    body {
+        color: white;
+        background-color: darkblue;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+# SynTeX Logo anzeigen
+st.image("syntex_logo.png", use_column_width=True)
+
+# Eingabefeld für Dateiupload
+uploaded_file = st.file_uploader("Datei hochladen")
+
+# Ladebalken anzeigen
+progress_bar = st.progress(0)
+
+# Ausgabetextfeld
+output_text = st.empty()
+
+# Tastendruck-Handler
+def on_keypress(key):
+    # Schriftfarbe ändern
+    if key == "r":
+        st.markdown(
+            """
+            <style>
+            body {
+                color: red !important;
+            }
+            </style>
+            """,
+            unsafe_allow_html=True,
+        )
+    elif key == "g":
+        st.markdown(
+            """
+            <style>
+            body {
+                color: green !important;
+            }
+            </style>
+            """,
+            unsafe_allow_html=True,
+        )
+    elif key == "b":
+        st.markdown(
+            """
+            <style>
+            body {
+                color: blue !important;
+            }
+            </style>
+            """,
+            unsafe_allow_html=True,
+        )
+
+# Tastendruck-Handler registrieren
+st.register_on_keydown_callback(on_keypress)
+
+# Beispieltext anzeigen
+st.write("Willkommen bei SynTeX!")
+
+# Dateiupload-Handler
+if uploaded_file:
+    with st.spinner("Datei wird hochgeladen..."):
+        # Dateiverarbeitung hier durchführen
+        # Beispiel: Fortschrittsanzeige aktualisieren
+        for percent_complete in range(100):
+            progress_bar.progress(percent_complete + 1)
+        progress_bar.empty()
+        output_text.write("Datei erfolgreich hochgeladen!")
